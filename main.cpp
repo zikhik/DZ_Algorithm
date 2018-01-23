aa046349d022384d7790039d94b812a13245921b
zikhik <slavyanskiji@gmail.com>
2018-01-23 19:37:22 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "speed_test.h"
 #include <iostream>
 #include "work_with_file.h"
+#include "correct.h"
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -16,26 +17,27 @@ int main(int argc, char* argv[])
     cout<< argv[1]<<endl;
     cout<< argv[2]<<endl;
     cout<< argv[3]<<endl;
+    
     if (argc !=4) return 0;
+    
     string in_f = argv[1];
     string out_f = argv[2];
     string mode_work = argv[3];
-    if ((mode_work != "file_test") && (mode_work != "speed_test")) return 0;
     
-    if (mode_work == "file_test")
-    {
-       // cout<<inp_from_file(in_f, out_f);
-    };
+    if ((mode_work != "file_test") && (mode_work != "speed_test") && (mode_work != "correct_test"))  return 0;
+    
+    if (mode_work == "file_test") cout<<inp_from_file(in_f, out_f);
+    
     if (mode_work == "speed_test")
     {
         test_1(out_f);
         test_2(out_f);
         test_34(out_f);
-        
-        
     };
    
-    
+    if (mode_work == "correct_test") correct_tests(out_f);
+
+
    
     
     
